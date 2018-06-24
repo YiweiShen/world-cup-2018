c71570e1f2942552abc0a3871e3c9293c03f7435
YiWeiShen <shenyiwei2013@gmail.com>
2018-06-24 23:07:33 +0800

    delete space

--- world_cup_2018.py
+++ world_cup_2018.py
@@ -9,19 +9,15 @@ def cal_country_and_score(game):
     home_team_goal = str(game['home_team']['goals'])
     away_team_country = str(game['away_team_country'])
     away_team_goal = str(game['away_team']['goals'])
-
     country_vs_info = str(home_team_country + ' Vs. ' + away_team_country)
     goal_vs_info = str(home_team_goal + '  :  ' + away_team_goal)
-
     return country_vs_info, goal_vs_info
 
 
 def main():
     game_now = requests.get('https://worldcup.sfg.io/matches/current').json()
-
     # pprint(game_now)
     # if you want to see all the statistics, pprint it all
-
     try:
         country_vs_info, goal_vs_info = cal_country_and_score(game_now[0])
         game_time = str(game_now[0]['time'])
@@ -51,8 +47,6 @@ def main():
         print(country_vs_info)
         # print(' ' * (len(home_team_country) - 1), end='')
         print(goal_vs_info)
-
-
     # test mac notification
         os.system("""
                   osascript -e 'display notification "{}" with title "{}"'
